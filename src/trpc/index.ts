963e0766206b9d01e40c7f748b0a7386ad2324cf
clemkofi <cak.nartey@gmail.com>
2023-10-15 21:54:58 +0200

    TRPC Setup

---
+++ src/trpc/index.ts
@@ -0,0 +1,7 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { publicProcedure, router } from "./trpc";
+import { TRPCError } from "@trpc/server";
+
+export const appRouter = router({});
+
+export type AppRouter = typeof appRouter;
