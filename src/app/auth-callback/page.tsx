fe2e5a5d95d816f62b9fa0bb3975f2af7bf9f4ad
clemkofi <cak.nartey@gmail.com>
2023-10-15 23:45:23 +0200

    Authentication and User Table in DB complete

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 /* 
 This is a page which is called when every someone tries to go the dashboard route 
 without logging in or when a user logs in for the very first time
@@ -5,6 +7,7 @@ without logging in or when a user logs in for the very first time
 
 import { useRouter, useSearchParams } from "next/navigation";
 import { trpc } from "../_trpc/client";
+import { Loader2 } from "lucide-react";
 
 const page = () => {
   const router = useRouter();
@@ -20,7 +23,24 @@ const page = () => {
         router.push(origin ? `/${origin}` : "/dashboard");
       }
     },
+    onError: (err) => {
+      if (err.data?.code === "UNAUTHORIZED") {
+        router.push("/api/auth/login?");
+      }
+    },
+    retry: true,
+    retryDelay: 500,
   });
+
+  return (
+    <div className="w-full mt-24 flex justify-center">
+      <div className="flex flex-col items-center gap-2">
+        <Loader2 className="h-8 w-8 animate-spin text-zinc-800" />
+        <h3 className="font-semibold text-xl">Setting up your account...</h3>
+        <p>You will be redirected automatically.</p>
+      </div>
+    </div>
+  );
 };
 
 export default page;
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,16 +1,32 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { publicProcedure, router } from "./trpc";
 import { TRPCError } from "@trpc/server";
+import { db } from "@/db";
 
 export const appRouter = router({
   // authcallback route to check if the user exists in the database
-  authCallback: publicProcedure.query(() => {
+  authCallback: publicProcedure.query(async () => {
     const { getUser } = getKindeServerSession();
     const user = getUser();
 
     if (!user.id || !user.email) throw new TRPCError({ code: "UNAUTHORIZED" });
 
     // check for whether the current user is synced in our db
+    const dbUser = await db.user.findFirst({
+      where: {
+        id: user.id,
+      },
+    });
+
+    // if user does not exist in db add the user to our db
+    if (!dbUser) {
+      await db.user.create({
+        data: {
+          id: user.id,
+          email: user.email,
+        },
+      });
+    }
 
     return { success: true };
   }),
