5d9e830aee169e11ff5fa7e3838933548b5560e1
clemkofi <cak.nartey@gmail.com>
2023-10-27 23:37:11 +0200

    Installed React PDF and added component for ChatWrapper and PDFRenderer

--- src/app/dashboard/[fileid]/page.tsx
+++ src/app/dashboard/[fileid]/page.tsx
@@ -1,12 +1,19 @@
 // import { trpc } from "@/app/_trpc/client";
+import ChatWrapper from "@/components/ChatWrapper";
+import PDFRenderer from "@/components/PDFRenderer";
 import { db } from "@/db";
-import { PageProps } from "@/lib/utils";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { notFound, redirect } from "next/navigation";
 
+interface PageProps {
+  params: {
+    fileid: string;
+  };
+}
+
 const page = async ({ params }: PageProps) => {
   // get the file id
-  const { fileid } = params!;
+  const { fileid } = params;
 
   //   check if the user is logged in
   const { getUser } = getKindeServerSession();
@@ -32,7 +39,23 @@ const page = async ({ params }: PageProps) => {
 
   if (!file) notFound();
 
-  return <div>{fileid}</div>;
+  return (
+    <div className="flex-1 justify-between flex flex-col h-[calc(100vh-3.5rem)]">
+      <div className="mx-auto w-full max-w-8xl grow lg:flex xl:px-2">
+        {/* Left sidebar & main wrapper */}
+        <div className="flex-1 xl:flex">
+          <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
+            {/* Main area */}
+            <PDFRenderer />
+          </div>
+        </div>
+
+        <div className="shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0">
+          <ChatWrapper />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default page;
